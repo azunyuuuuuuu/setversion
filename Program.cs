d67de6204aeb1dfaa6eeca034da911e19692978c
Alice Triebull <azunyuuuuuuu@users.noreply.github.com>
2024-11-24 14:59:46 +0100

    code: henlo worl :3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Cocona;
+using Spectre.Console;
+
+var app = CoconaLiteApp.Create();
+
+app.AddCommand(() =>
+{
+    AnsiConsole.MarkupLine("[underline green]henlo[/] worl :3");
+});
+
+app.Run();
